73160dd1a38bbf3eea71fec81429b4f6f8442d13
reinaldoper <reinaldoper83@gmail.com>
2024-06-13 15:32:25 -0400

    feat: adiciona um pouco do layout da home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,16 +4,34 @@ import orkut_png from '../assets/orkut1.webp'
 function Home() {
 
   return (
-    <>
+    <div className='container'>
+      <h1>Here goes the navigation page</h1>
       <div className='content-page'>
         <div>
-          <img className='orkut-img' src={orkut_png} alt="orkut" />
+          <img className='orkut-img' src={orkut_png} alt="orkut" title='orkut-page' />
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <div className="read-the-docs">
+          <form method="submit" className='login'>
+            <h3>login</h3>
+            <h4>Acesse o orkut com sua conta do Google</h4>
+            <label htmlFor="">
+              E-mail:
+              <input type="email" className='email' />
+            </label>
+            <label htmlFor="">
+              Senha:
+              <input type="password" className='password' />
+            </label>
+            <label htmlFor="">
+              <input type="checkbox" className='checkbox'/>
+              Salvar as minhas informações neste computador
+            </label>
+            <button type="submit" className='button-login'>Login</button>
+          </form>
+          
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
