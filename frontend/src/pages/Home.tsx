c034351ef3561ffe7bf8df7a16cdc4aeed6576b2
reinaldoper <reinaldoper83@gmail.com>
2024-06-13 16:08:35 -0400

    refactored: ajusta css da home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,38 +1,36 @@
-import '../styles/App.css'
-import orkut_png from '../assets/orkut1.webp'
+import '../styles/App.css';
+import orkut_png from '../assets/orkut1.webp';
 
 function Home() {
-
   return (
-    <div className='container'>
-      <h1>Here goes the navigation page</h1>
-      <div className='content-page'>
-        <div>
-          <img className='orkut-img' src={orkut_png} alt="orkut" title='orkut-page' />
+    <div className="container">
+      <h1>Orkut</h1>
+      <div className="content-page">
+        <div className="image-container">
+          <img className="orkut-img" src={orkut_png} alt="orkut" title="orkut-page" />
         </div>
-        <div className="read-the-docs">
-          <form method="submit" className='login'>
-            <h3>login</h3>
-            <h4>Acesse o orkut com sua conta do Google</h4>
-            <label htmlFor="">
+        <div className="login-container">
+          <form method="submit" className="login">
+            <h3>Login</h3>
+            <h4>Acesse o Orkut com sua conta do Google</h4>
+            <label>
               E-mail:
-              <input type="email" className='email' />
+              <input type="email" className="email" />
             </label>
-            <label htmlFor="">
+            <label>
               Senha:
-              <input type="password" className='password' />
+              <input type="password" className="password" />
             </label>
-            <label htmlFor="">
-              <input type="checkbox" className='checkbox'/>
+            <label className="checkbox-container">
+              <input type="checkbox" className="checkbox" />
               Salvar as minhas informações neste computador
             </label>
-            <button type="submit" className='button-login'>Login</button>
+            <button type="submit" className="button-login">Login</button>
           </form>
-          
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
