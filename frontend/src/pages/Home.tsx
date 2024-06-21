dde64955929b1392f912f6c329197af219b85cb8
reinaldoper <reinaldoper83@gmail.com>
2024-06-21 11:25:39 -0400

    feat: altera HomePage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,6 +11,7 @@ function Home() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  
 
   const navigate = useNavigate();
 
