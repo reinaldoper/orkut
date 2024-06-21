b4221938d7137dd1474a42651db74e5ba4e8144e
reinaldoper <reinaldoper83@gmail.com>
2024-06-21 11:25:21 -0400

    feat: ajusta contentPage

--- frontend/src/pages/ContentPage.tsx
+++ frontend/src/pages/ContentPage.tsx
@@ -1,11 +1,13 @@
 import Content from "../components/Content"
 import '../styles/Content.css'
+import NavLink from "../components/NavLink"
 
 const ContentPage = () => {
   return (
-    <>
+    <div>
+      <NavLink />
       <Content />
-    </>
+    </div>
   )
 }
 
