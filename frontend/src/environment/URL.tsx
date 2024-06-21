6caa36461eccf79349f7611e20fa1091ee53bfd1
reinaldoper <reinaldoper83@gmail.com>
2024-06-21 11:25:07 -0400

    feat: adiciona URL

--- frontend/src/environment/URL.tsx
+++ frontend/src/environment/URL.tsx
@@ -1,1 +1,3 @@
-export const URL = 'http://localhost:3000/users';
+export const URL_USER = 'http://localhost:3000/users';
+export const URL_CATEGORY = 'http://localhost:3000/category';
+export const URL_POSTS = 'http://localhost:3000/posts';
