c12ea18c27d20ad7cb37d024106bff5df9efd0d8
reinaldoper <reinaldoper83@gmail.com>
2024-07-04 11:09:51 -0400

    refactored: ajusta css do listPost

--- frontend/src/components/ListPost.tsx
+++ frontend/src/components/ListPost.tsx
@@ -107,17 +107,17 @@ const ListPost = () => {
       <div className="hover:ps-10 hover:pe-10">
         {MessagePost()}
       </div>
-      <div className="max-h-96 overflow-y-scroll scrollbar-hide">
+      <div className="max-h-screen overflow-y-scroll scrollbar-hide">
         {posts?.map((post) => (
-          <div key={post.id} className="bg-white p-6 mt-4 rounded-lg shadow-md">
-            <div className="flex mb-5">
+          <div key={post.id} className="bg-slate-400 p-6 mt-4 rounded-lg shadow-md">
+            <div className="flex mb-5 bg-sky-300 rounded-2xl">
               <ReqUserById id={post.userId} />
               <h1 className="text-blue-600 mb-2 mt-5">{post.title}</h1>
             </div>
             <p className="mb-4">{post.content}</p>
             <div className="flex flex-wrap">
               {post.photos.length > 0 && post.photos.map((photo) => (
-                <div key={photo.id} className="bg-slate-400">
+                <div key={photo.id}>
                   <img className="w-32 h-32 rounded-3xl p-2 hover:scale-150" src={`${SRC}${photo.url ?? ''}`} alt={photo.title} />
                 </div>
               ))}
@@ -127,7 +127,7 @@ const ListPost = () => {
                 <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                   <path fillRule="evenodd" d="M15.03 9.684h3.965c.322 0 .64.08.925.232.286.153.532.374.717.645a2.109 2.109 0 0 1 .242 1.883l-2.36 7.201c-.288.814-.48 1.355-1.884 1.355-2.072 0-4.276-.677-6.157-1.256-.472-.145-.924-.284-1.348-.404h-.115V9.478a25.485 25.485 0 0 0 4.238-5.514 1.8 1.8 0 0 1 .901-.83 1.74 1.74 0 0 1 1.21-.048c.396.13.736.397.96.757.225.36.32.788.269 1.211l-1.562 4.63ZM4.177 10H7v8a2 2 0 1 1-4 0v-6.823C3 10.527 3.527 10 4.176 10Z" clipRule="evenodd" />
                 </svg>
-                <span>{post.likes}</span>
+                <span>({post.likes})</span>
               </button>
               <span className="text-sm mx-10 flex text-gray-500 hover:text-red-700">
                 <svg className="w-6 h-10" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
